62f2f2c0b9c0c48268b5f2e529c24316adf3ce80
Luis Moreno <22335041+morenol@users.noreply.github.com>
2021-06-07 12:28:35 -0400

    Export types (#20)
    Co-authored-by: Luis Moreno <morenol@users.noreply.github.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
 import rust from "@wasm-tool/rollup-plugin-rust";
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
-export default {
+const config = [
+  {
     input: "index.ts",
     output: {
-        file: "dist/fluvio-client.js",
-        format: "umd",
-        sourcemap: true,
-	name: "fluvio",
+      file: "dist/fluvio-client.js",
+      format: "umd",
+      sourcemap: true,
+      name: "fluvio",
     },
     plugins: [
-        rust({
-	    watchPatterns: ["src/**"],
-	}),
-	typescript()
+      rust({
+	  serverPath: "/",
+      }),
+      typescript(),
     ],
-};
+  },
+  {
+    input: "index.ts",
+    output: [{ file: "dist/fluvio-client.d.ts", format: "es" }],
+    plugins: [rust(), dts()],
+  },
+];
+
+export default config;
