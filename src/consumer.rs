5b8964679cffca654b809276616694a71bceb5ab
Nick Mosher <nicholastmosher@gmail.com>
2021-06-14 17:41:24 -0400

    Add ConsumerConfig and stream_with_config for using SmartStreams

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,4 +1,7 @@
-use fluvio::PartitionConsumer as NativePartitionConsumer;
+use fluvio::{
+    ConsumerConfig as NativeConsumerConfig, ConsumerConfigBuilder,
+    PartitionConsumer as NativePartitionConsumer,
+};
 use js_sys::Promise;
 use std::cell::RefCell;
 use std::pin::Pin;
@@ -9,6 +12,36 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
 
 use crate::{FluvioError, Offset, Record};
+
+#[wasm_bindgen]
+pub struct ConsumerConfig {
+    inner: Rc<RefCell<Pin<Box<ConsumerConfigBuilder>>>>,
+}
+
+#[wasm_bindgen]
+impl ConsumerConfig {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+            inner: Rc::new(RefCell::new(Box::pin(NativeConsumerConfig::builder()))),
+        }
+    }
+
+    #[wasm_bindgen(setter, js_name = "maxBytes")]
+    pub fn set_max_bytes(&mut self, max_bytes: i32) {
+        self.inner.borrow_mut().max_bytes(max_bytes);
+    }
+
+    #[wasm_bindgen(setter, js_name = "wasmFilterBinary")]
+    pub fn set_wasm_filter_binary(&mut self, binary: Vec<u8>) {
+        self.inner.borrow_mut().smartstream_binary(binary);
+    }
+
+    #[wasm_bindgen(setter, js_name = "wasmFilterBase64")]
+    pub fn set_wasm_filter_base64(&mut self, base64: String) {
+        self.inner.borrow_mut().smartstream_base64(base64).unwrap();
+    }
+}
 
 #[wasm_bindgen]
 pub struct PartitionConsumerStream {
@@ -52,7 +85,30 @@ impl PartitionConsumer {
             ))),
         })
     }
+
+    #[wasm_bindgen(js_name = "streamWithConfig")]
+    pub async fn stream_with_config(
+        self,
+        offset: Offset,
+        config: ConsumerConfig,
+    ) -> Result<PartitionConsumerStream, FluvioError> {
+        let config: NativeConsumerConfig = config.inner.borrow().build().unwrap();
+
+        Ok(PartitionConsumerStream {
+            inner: Rc::new(RefCell::new(Box::pin(
+                self.inner
+                    .stream_with_config(offset.inner, config)
+                    .await?
+                    .map(|result| {
+                        result
+                            .map(|record| record.into())
+                            .map_err(FluvioError::from)
+                    }),
+            ))),
+        })
+    }
 }
+
 impl From<NativePartitionConsumer> for PartitionConsumer {
     fn from(inner: NativePartitionConsumer) -> Self {
         Self { inner }
