312bc296f69cba46e6d2a229b118630e0b954b61
Nick Mosher <nicholastmosher@gmail.com>
2021-06-18 14:16:51 -0400

    Add ConsumerConfig and stream_with_config for using SmartStreams (#35)
    * Add ConsumerConfig and stream_with_config for using SmartStreams
    
    * Add object initializer for ConsumerConfig
    
    * Use old ConsumerConfig pattern

---
+++ js/test-smartstreams.js
@@ -0,0 +1,62 @@
+// In this example, we produce and consume records from Topic "foobar"
+// We send the following records:
+//
+// ("", "Record with a")
+// ("", "little 'a', count 0")
+// ("", "BIG 'A', count 1")
+// ("", "little 'a', count 2")
+// ("", "BIG 'A', count 3")
+// ("", "little 'a', count 4")
+// ("", "BIG 'A', count 5")
+// ("", "little 'a', count 6")
+// ("", "BIG 'A', count 7")
+// ("", "little 'a', count 8")
+// ("", "BIG 'A', count 9")
+//
+// However, in this example we are also applying a SmartStream Filter
+// to the consumer. This filter keeps only records that contain an 'a'
+// in them. If everything works correctly, you should see the following
+// records appear when running the example:
+//
+// KEY: , VALUE: Record with a
+// KEY: , VALUE: little 'a', count 0
+// KEY: , VALUE: little 'a', count 2
+// KEY: , VALUE: little 'a', count 4
+// KEY: , VALUE: little 'a', count 6
+// KEY: , VALUE: little 'a', count 8
+
+import("../pkg").then(async ({Fluvio, Offset, ConsumerConfig}) => {
+  const TOPIC = "foobar";
+  const fluvio = await Fluvio.connect("ws://localhost:3000")
+
+  const producer = await fluvio.topicProducer(TOPIC);
+  await producer.send("", 'Record with a');
+
+  // Set up Consumer using a SmartStream filter.
+  // This filter keeps only Records whose value contains an 'a'.
+  const consumer = await fluvio.partitionConsumer(TOPIC, 0);
+
+  const {filter} = await import("./smartstream.js");
+  const config = new ConsumerConfig({
+    smartstreamFilter: filter,
+  });
+  let stream = await consumer.streamWithConfig(Offset.beginning(), config)
+
+  let count = 0;
+  let before = new Date();
+  while (count < 10) {
+
+    // Every other iteration, send a record with 'a'
+    if (count % 2 === 0) {
+      await producer.send("", `little 'a', count ${count}`);
+    } else {
+      await producer.send("", `BIG 'A', COUNT ${count}`);
+    }
+
+    let next = await stream.next();
+    console.log(`KEY: ${next.keyString()}, VALUE: ${next.valueString()}`);
+    count++;
+  }
+  let after = new Date();
+  console.log(`The received ${count} in took ${after - before} ms`);
+});
--- src/consumer.rs
+++ src/consumer.rs
@@ -1,4 +1,6 @@
-use fluvio::PartitionConsumer as NativePartitionConsumer;
+use fluvio::{
+    ConsumerConfig as NativeConsumerConfig, PartitionConsumer as NativePartitionConsumer,
+};
 use js_sys::Promise;
 use std::cell::RefCell;
 use std::pin::Pin;
@@ -9,6 +11,61 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
 
 use crate::{FluvioError, Offset, Record};
+use std::convert::{TryFrom, TryInto};
+
+#[wasm_bindgen]
+pub struct ConsumerConfig {
+    max_bytes: Option<i32>,
+    smartstream_filter: Option<String>,
+}
+
+#[wasm_bindgen]
+impl ConsumerConfig {
+    #[wasm_bindgen(constructor)]
+    pub fn new(js: JsValue) -> Self {
+        let max_bytes = js_sys::Reflect::get(&js, &"maxBytes".into())
+            .ok()
+            .and_then(|it| it.as_f64())
+            .map(|it| it.round() as i32);
+
+        let smartstream_filter = js_sys::Reflect::get(&js, &"smartstreamFilter".into())
+            .ok()
+            .and_then(|it| it.as_string());
+
+        Self {
+            max_bytes,
+            smartstream_filter,
+        }
+    }
+
+    #[wasm_bindgen(setter, js_name = "maxBytes")]
+    pub fn set_max_bytes(&mut self, max_bytes: i32) {
+        self.max_bytes = Some(max_bytes);
+    }
+
+    #[wasm_bindgen(setter, js_name = "smartstreamFilter")]
+    pub fn set_smartstream_filter(&mut self, string: String) {
+        self.smartstream_filter = Some(string);
+    }
+}
+
+impl TryFrom<ConsumerConfig> for NativeConsumerConfig {
+    type Error = String;
+
+    fn try_from(value: ConsumerConfig) -> Result<Self, Self::Error> {
+        let mut config = NativeConsumerConfig::default();
+        if let Some(max_bytes) = value.max_bytes {
+            config = config.with_max_bytes(max_bytes);
+        }
+        if let Some(wasm_base64) = value.smartstream_filter {
+            let wasm = base64::decode(wasm_base64)
+                .map_err(|e| format!("Failed to decode SmartStream as a base64 string: {:?}", e))?;
+            config = config.with_wasm_filter(wasm);
+        }
+
+        Ok(config)
+    }
+}
 
 #[wasm_bindgen]
 pub struct PartitionConsumerStream {
@@ -53,7 +110,30 @@ impl PartitionConsumer {
             ))),
         })
     }
+
+    #[wasm_bindgen(js_name = "streamWithConfig")]
+    pub async fn stream_with_config(
+        self,
+        offset: Offset,
+        config: ConsumerConfig,
+    ) -> Result<PartitionConsumerStream, FluvioError> {
+        let config: NativeConsumerConfig = config.try_into()?;
+
+        Ok(PartitionConsumerStream {
+            inner: Rc::new(RefCell::new(Box::pin(
+                self.inner
+                    .stream_with_config(offset.inner, config)
+                    .await?
+                    .map(|result| {
+                        result
+                            .map(|record| record.into())
+                            .map_err(FluvioError::from)
+                    }),
+            ))),
+        })
+    }
 }
+
 impl From<NativePartitionConsumer> for PartitionConsumer {
     fn from(inner: NativePartitionConsumer) -> Self {
         Self { inner }
