5070a66e2e361d0109b71300f87e4441c40ee7dc
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-09-30 09:20:27 -0400

    Updated dependencies and cleanup tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,23 @@ pub use record::Record;
 // allocator.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use wasm_bindgen::convert::FromWasmAbi;
+use js_sys::Reflect;
+use wasm_bindgen::JsValue;
+
+pub(crate) fn generic_of_jsval<T: FromWasmAbi<Abi = u32>>(
+    js: JsValue,
+    classname: &str,
+) -> Result<T, JsValue> {
+    use js_sys::Object;
+    let ctor_name = Object::get_prototype_of(&js).constructor().name();
+    if ctor_name == classname {
+        let ptr = Reflect::get(&js, &JsValue::from_str("ptr"))?;
+        let ptr_u32: u32 = ptr.as_f64().ok_or(JsValue::NULL)? as u32;
+        let foo = unsafe { T::from_abi(ptr_u32) };
+        Ok(foo)
+    } else {
+        Err(JsValue::NULL)
+    }
+}
--- tests/js_tests.rs
+++ tests/js_tests.rs
@@ -3,12 +3,16 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
+use std::convert::TryFrom;
 
 pub use fluvio_client_wasm::*;
 
 async fn get_fluvio() -> Fluvio {
     let fluvio = Fluvio::connect("ws://localhost:3000".into()).await;
-    assert!(fluvio.is_ok());
+    if let Err(e) = fluvio {
+        let e = FluvioError::try_from(e);
+        panic!("{:?}", e);
+    }
     fluvio.unwrap()
 }
 
@@ -27,27 +31,29 @@ async fn simple() {
         pub async fn teardown(fluvio: Fluvio) -> Result<JsValue, JsValue>;
     }
 
-    setup(get_fluvio().await).await.unwrap();
-    test(get_fluvio().await, Offset::from_end(1)).await.unwrap();
-    teardown(get_fluvio().await).await.unwrap();
+    setup(get_fluvio().await).await.map_err(FluvioError::try_from).expect("Setup failed");
+    test(get_fluvio().await, Offset::from_end(1)).await.map_err(FluvioError::try_from).expect("Test failed");
+    teardown(get_fluvio().await).await.map_err(FluvioError::try_from).expect("Teardown failed");
 }
 
-// #[wasm_bindgen_test]
-// async fn smartstream_filter() {
-//     #[wasm_bindgen(module = "/tests/js/smartstream_filter/smartstream_filter.js")]
-//     extern "C" {
-//
-//         #[wasm_bindgen(catch)]
-//         pub async fn setup(fluvio: Fluvio) -> Result<JsValue, JsValue>;
-//
-//         #[wasm_bindgen(catch)]
-//         pub async fn test(fluvio: Fluvio, offset: Offset) -> Result<JsValue, JsValue>;
-//
-//         #[wasm_bindgen(catch)]
-//         pub async fn teardown(fluvio: Fluvio) -> Result<JsValue, JsValue>;
-//     }
-//
-//     setup(get_fluvio().await).await.unwrap();
-//     test(get_fluvio().await, Offset::from_end(1)).await.unwrap();
-//     teardown(get_fluvio().await).await.unwrap();
-// }
+/*
+#[wasm_bindgen_test]
+async fn smartstream_filter() {
+    #[wasm_bindgen(module = "/tests/js/smartstream_filter/smartstream_filter.js")]
+    extern "C" {
+
+        #[wasm_bindgen(catch)]
+        pub async fn setup(fluvio: Fluvio) -> Result<JsValue, JsValue>;
+
+        #[wasm_bindgen(catch)]
+        pub async fn test(fluvio: Fluvio, offset: Offset) -> Result<JsValue, JsValue>;
+
+        #[wasm_bindgen(catch)]
+        pub async fn teardown(fluvio: Fluvio) -> Result<JsValue, JsValue>;
+    }
+
+    setup(get_fluvio().await).await.map_err(FluvioError::try_from).expect("Setup failed");
+    test(get_fluvio().await, Offset::from_end(1)).await.map_err(FluvioError::try_from).expect("Test failed");
+    teardown(get_fluvio().await).await.map_err(FluvioError::try_from).expect("Teardown failed");
+}
+*/
