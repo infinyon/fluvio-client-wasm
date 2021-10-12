24f8fa6ea8a823cd58794fb4d75485afa7a977c7
simlay <simlay@users.noreply.github.com>
2021-10-12 11:46:45 -0700

    Added map tests to ci (#141)

--- tests/js/utils.js
+++ tests/js/utils.js
@@ -1,4 +1,3 @@
-
 export const createUUID = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
