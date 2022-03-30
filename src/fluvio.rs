5857a5ab2a333da25ef02de1d1fe2acbfd440f28
morenol <morenol@users.noreply.github.com>
2022-03-30 16:24:27 +0000

    Add producer config (#292)
    Fixes #293

--- src/fluvio.rs
+++ src/fluvio.rs
@@ -1,14 +1,72 @@
 use std::rc::Rc;
 
+use js_sys::Reflect;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::future_to_promise;
 
-use fluvio::{config::FluvioConfig, Fluvio as NativeFluvio, PartitionSelectionStrategy};
+use fluvio::{
+    config::FluvioConfig, Compression, Fluvio as NativeFluvio, PartitionSelectionStrategy,
+    TopicProducerConfig as NativeTopicProducerConfig,
+    TopicProducerConfigBuilder as NativeTopicProducerConfigBuilder,
+};
 
 use crate::{
     FluvioAdmin, FluvioError, FluvioWebsocketConnector, MultiplePartitionConsumer,
     PartitionConsumer, TopicProducer,
 };
+
+#[wasm_bindgen(typescript_custom_section)]
+const PRODUCER_CONFIG_TYPE: &str = r#"
+export type CompressionAlgorithm = "none" | "gzip" | "snappy" | "lz4";
+export type TopicProducerConfig = {
+    batchSize?: number,
+    lingerTime?: number,
+    compression?: CompressionAlgorithm,
+}
+"#;
+
+impl TryFrom<TopicProducerConfig> for NativeTopicProducerConfig {
+    type Error = String;
+
+    fn try_from(config: TopicProducerConfig) -> Result<Self, Self::Error> {
+        let mut builder = NativeTopicProducerConfigBuilder::default();
+        let batch_size = Reflect::get(&config, &"batchSize".into())
+            .ok()
+            .and_then(|it| it.as_f64())
+            .map(|it| it.round() as usize);
+
+        if let Some(batch_size) = batch_size {
+            builder = builder.batch_size(batch_size);
+        }
+
+        let linger_time = Reflect::get(&config, &"lingerTime".into())
+            .ok()
+            .and_then(|it| it.as_f64())
+            .map(|it| std::time::Duration::from_millis(it.round() as u64));
+
+        if let Some(linger_time) = linger_time {
+            builder = builder.linger(linger_time);
+        }
+
+        let compression = match Reflect::get(&config, &"compression".into())
+            .ok()
+            .and_then(|it| it.as_string())
+        {
+            Some(compression) => Some(
+                compression
+                    .parse::<Compression>()
+                    .map_err(|e| e.to_string())?,
+            ),
+            None => None,
+        };
+
+        if let Some(compression) = compression {
+            builder = builder.compression(compression);
+        }
+
+        builder.build().map_err(|e| e.to_string())
+    }
+}
 
 // Workaround for Typescript type annotations on async function returns.
 #[wasm_bindgen]
@@ -24,6 +82,12 @@ extern "C" {
 
     #[wasm_bindgen(typescript_type = "Promise<FluvioAdmin>")]
     pub type PromiseFluvioAdmin;
+
+    #[wasm_bindgen(typescript_type = "CompressionAlgorithm")]
+    pub type CompressionAlgorithm;
+    #[wasm_bindgen(typescript_type = "TopicProducerConfig")]
+    pub type TopicProducerConfig;
+
 }
 
 #[wasm_bindgen]
@@ -40,6 +104,27 @@ impl Fluvio {
 
         let promise = future_to_promise(async move {
             rc.topic_producer(topic)
+                .await
+                .map(|producer| JsValue::from(TopicProducer::from(producer)))
+                .map_err(|e| (FluvioError::from(e).into()))
+        });
+
+        // WARNING: this does not validate the return type. Check carefully.
+        promise.unchecked_into::<PromiseTopicProducer>()
+    }
+
+    #[wasm_bindgen(js_name = topicProducerWithConfig)]
+    pub fn topic_producer_with_config(
+        &self,
+        topic: String,
+        config: TopicProducerConfig,
+    ) -> PromiseTopicProducer {
+        let rc = self.inner.clone();
+
+        let promise = future_to_promise(async move {
+            let config: NativeTopicProducerConfig = config.try_into()?;
+
+            rc.topic_producer_with_config(topic, config)
                 .await
                 .map(|producer| JsValue::from(TopicProducer::from(producer)))
                 .map_err(|e| (FluvioError::from(e).into()))
